791d07a305d102a4756532a1f6038a63e4e4c3e4
HarshilK <HarshilK@TRains106.thoughtrains.local>
2023-05-16 17:40:29 +0530

    code formated

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,7 +69,7 @@ new simpleParallax(image2, {
 
 
 
- // SCROLLING NAV
+// SCROLLING NAV
 (function ($) {
   "use strict"; // Start of use strict
 
@@ -90,13 +90,13 @@ new simpleParallax(image2, {
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
-      $('.navbar-collapse').collapse('hide');
+    $('.navbar-collapse').collapse('hide');
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-      target: '#mainNav',
-      offset: 100
+    target: '#mainNav',
+    offset: 100
   });
 
 })(jQuery); // End of use strict
